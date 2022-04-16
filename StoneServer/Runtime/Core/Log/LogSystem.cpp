1f50293f5a392e264de9caa8b27ed0ecbc9c12a7
ImGili <18017158729@163.com>
2022-04-16 23:47:14 +0800

    ✨ feat: 困 暂时到这里

--- StoneServer/Runtime/Core/Log/LogSystem.cpp
+++ StoneServer/Runtime/Core/Log/LogSystem.cpp
@@ -1,12 +1,10 @@
 #include "Core/Log/LogSystem.h"
-#include <cstdio>
+#include <cstring>
 #include <time.h>
-#include <string.h>
-#include <iostream>
-#include <stdarg.h>
+#include <stdio.h>
 namespace Stone
 {
-    void LogSystem::Init(const char* filename)
+    bool LogSystem::Init(const char* filename)
     {
         time_t t = time(NULL);
         tm* systemtm = localtime(&t);
@@ -24,6 +22,37 @@ namespace Stone
             strncpy(dir_name, filename, p - filename + 1);
             snprintf(log_full_name, 255, "%s%d_%02d_%02d_%s", dir_name, nowtime.tm_year + 1900, nowtime.tm_mon + 1, nowtime.tm_mday, log_name);
         }
-        printf("%s\n", log_full_name);
+
+        auto m_fp = std::fopen(log_full_name, "a");
+
+        if (m_fp == NULL) {
+            return false;
+        }
+
+        return true;
     }
+
+    void WriteLog(int level, const char* format, ...)
+    {
+
+        char s[16] = {0};
+        switch (level) {
+            case 0:
+                strcpy(s, "[debug]");
+                break;
+            case 1:
+                strcpy(s, "[info]");
+                break;
+            case 2:
+                strcpy(s, "[warn]");
+                break;
+            case 3:
+                strcpy(s, "[erro]");
+                break;
+            default:
+                strcpy(s, "[info]");
+                break;
+        }
+    }
+
 } // namespace GU
--- StoneServer/Runtime/Core/Log/LogSystem.h
+++ StoneServer/Runtime/Core/Log/LogSystem.h
@@ -1,16 +1,19 @@
 #pragma once
 #include "Core/Base/PublicSingleton.h"
-
+#include <stdarg.h>
+#include <string.h>
+#include <iostream>
 namespace Stone
 {
     class LogSystem : public PublicSingleton<LogSystem>
     {
     public:
-        void Init(const char* file_name);
+        bool Init(const char* file_name);
         
-        void WriteLog(int level, const char* format, ...) {}
+        void WriteLog(int level, const char* format, ...);
     private:
         char log_name[128];
         char dir_name[128]; //路径名
+        FILE* m_fp;
     };
 } // namespace GU
